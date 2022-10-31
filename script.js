fc3706a5c2eca31a6ced0da3df89f4d9a156df4e
Aaron <stephenaaroneaves@gmail.com>
2022-10-31 00:43:36 -0400

    Number buttons and clear button now populate display

--- script.js
+++ script.js
@@ -28,3 +28,59 @@ const doTheMaths = function (a, b, operator) {
     }
 };
 
+const display = document.querySelector('#display');
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => {
+    display.textContent = '';
+});
+
+const btn0 = document.querySelector('#btn0');
+btn0.addEventListener('click', () => {
+    display.textContent += 0;
+});
+
+const btn1 = document.querySelector('#btn1');
+btn1.addEventListener('click', () => {
+    display.textContent += 1;
+});
+
+const btn2 = document.querySelector('#btn2');
+btn2.addEventListener('click', () => {
+    display.textContent += 2;
+});
+
+const btn3 = document.querySelector('#btn3');
+btn3.addEventListener('click', () => {
+    display.textContent += 3;
+});
+
+const btn4 = document.querySelector('#btn4');
+btn4.addEventListener('click', () => {
+    display.textContent += 4;
+});
+
+const btn5 = document.querySelector('#btn5');
+btn5.addEventListener('click', () => {
+    display.textContent += 5;
+});
+
+const btn6 = document.querySelector('#btn6');
+btn6.addEventListener('click', () => {
+    display.textContent += 6;
+});
+
+const btn7 = document.querySelector('#btn7');
+btn7.addEventListener('click', () => {
+    display.textContent += 7;
+});
+
+const btn8 = document.querySelector('#btn8');
+btn8.addEventListener('click', () => {
+    display.textContent += 8;
+});
+
+const btn9 = document.querySelector('#btn9');
+btn9.addEventListener('click', () => {
+    display.textContent += 9;
+});

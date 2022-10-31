048769ff4949c6e67adb13af46d3d2df95679bab
Aaron <stephenaaroneaves@gmail.com>
2022-10-30 22:31:11 -0400

    Have base functions working (i.e. add, subtract, multiply, divide)

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('my man');
+console.log('JS file is still working');
+
+const add = function (a, b) {
+    return a + b;
+};
+
+const subtract = function (a, b) {
+    return a - b;
+};
+
+const multiply = function (a, b) {
+    return a * b;
+};
+
+const divide = function (a, b) {
+    return a / b;
+};
